80daa91f9908786c04cb1cff538e62ce5aca91a0
duythien2610 <33doduythien@gmail.com>
2026-08-25 14:14:14 +0700

    fix(custom-link): update action tooltips to 'Cấu hình SP/DV' and 'Sao chép link' with cache busting v10

--- ba-tool-kit/project/AM/custom-link-nang-cao/demo/app.js
+++ ba-tool-kit/project/AM/custom-link-nang-cao/demo/app.js
@@ -522,13 +522,13 @@ function renderLinksTable(data, startIndex, totalRecords) {
                             <circle cx="12" cy="12" r="3"></circle>
                         </svg>
                     </button>
-                    <button class="btn-icon-action" title="Chỉnh sửa & Tùy biến SP/DV" onclick="openEditForm('${link.id}')">
+                    <button class="btn-icon-action" title="Cấu hình SP/DV" onclick="openEditForm('${link.id}')">
                         <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                             <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
                             <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
                         </svg>
                     </button>
-                    <button class="btn-icon-action" title="Sao chép Shortlink" onclick="copyShortlink('${link.shortlink}')">
+                    <button class="btn-icon-action" title="Sao chép link" onclick="copyShortlink('${link.shortlink}')">
                         <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                             <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
                             <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
